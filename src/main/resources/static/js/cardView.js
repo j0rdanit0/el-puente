a81ea5e1ff6a18b6dcefaf89204bd9a308f547da
Jordan Simpson <Jordan.Simpson@ABCFinancial.com>
2019-11-13 15:55:12 -0600

    Split after school programs and summer programs.

--- src/main/resources/static/js/cardView.js
+++ src/main/resources/static/js/cardView.js
@@ -4,6 +4,7 @@ $(document).ready( function() {
          .closest('.cardViewContainer').each( function() {
          $(this).find('.displayCards').addClass( 'card-columns' );
          $(this).find('.displayInRowsButton').removeClass( 'text-primary' );
+         $(this).find('.cardImageContainer').removeClass('col-3').addClass('col-12');
       });
    });
 
@@ -12,6 +13,7 @@ $(document).ready( function() {
          .closest('.cardViewContainer').each( function() {
          $(this).find('.displayCards').removeClass( 'card-columns' );
          $(this).find('.displayInColumnsButton').removeClass( 'text-primary' );
+         $(this).find('.cardImageContainer').removeClass('col-12').addClass('col-3');
       });
    });
 });
