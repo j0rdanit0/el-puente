2cd5c940d2d3b42cc71c6c32e7185f0a8c79a8c8
Jordan Simpson <Jordan.Simpson@ABCFinancial.com>
2019-11-13 13:25:05 -0600

    Removed Salsa competition things.

--- src/main/resources/static/js/changes.js
+++ src/main/resources/static/js/changes.js
@@ -1,12 +1,25 @@
 $(function () {
    $('#goLiveButton').click( function() {
       const $goLiveButton = $(this);
-      $goLiveButton.addClass( 'disabled' );
       const $loadingIcon = $goLiveButton.find( '.spinner-border' );
-      $loadingIcon.removeClass( 'd-none' );
-      $.post( '/changes/goLive', {}, function() {
-         $loadingIcon.addClass( 'd-none' );
-         $goLiveButton.removeClass( 'disabled' );
-      });
+      goLiveInProgress( $goLiveButton, $loadingIcon );
+      if ( confirm( 'Are you sure you want to Go Live? This cannot be undone.' ) ) {
+         $.post( '/changes/goLive', {}, function() {
+            goLiveFinished( $goLiveButton, $loadingIcon );
+         });
+      }
+      else {
+         goLiveFinished( $goLiveButton, $loadingIcon );
+      }
    });
 });
+
+function goLiveInProgress( $goLiveButton, $loadingIcon ) {
+   $goLiveButton.addClass( 'disabled' );
+   $loadingIcon.removeClass( 'd-none' );
+}
+
+function goLiveFinished( $goLiveButton, $loadingIcon ) {
+   $loadingIcon.addClass( 'd-none' );
+   $goLiveButton.removeClass( 'disabled' );
+}
