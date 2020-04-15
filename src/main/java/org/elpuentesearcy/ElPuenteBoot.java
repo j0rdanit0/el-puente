29948e47c87f32b1dc1b0b3b8930928ee57a602a
Jordan Simpson <jordan.simpson.dev@gmail.com>
2020-04-15 17:32:31 -0500

    Added logging for image directory

--- src/main/java/org/elpuentesearcy/ElPuenteBoot.java
+++ src/main/java/org/elpuentesearcy/ElPuenteBoot.java
@@ -1,5 +1,7 @@
 package org.elpuentesearcy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +11,8 @@ import java.io.IOException;
 @SpringBootApplication
 public class ElPuenteBoot
 {
+    private static final Logger logger = LoggerFactory.getLogger( ElPuenteBoot.class );
+
     public static final String IMAGE_FOLDER = "/images/";
     public static final String DOWNLOADS_FOLDER = "/downloads/";
     public static final String WELL_KNOWN_FOLDER = "/.well-known/";
@@ -20,8 +24,11 @@ public class ElPuenteBoot
     {
         String basePath = new File( "." ).getCanonicalPath();
         IMAGE_DIRECTORY = basePath + IMAGE_FOLDER;
+        logger.info( "IMAGE_DIRECTORY: " + IMAGE_DIRECTORY );
         DOWNLOADS_DIRECTORY = basePath + DOWNLOADS_FOLDER;
+        logger.info( "DOWNLOADS_DIRECTORY: " + DOWNLOADS_DIRECTORY );
         WELL_KNOWN_DIRECTORY = basePath + WELL_KNOWN_FOLDER;
+        logger.info( "WELL_KNOWN_DIRECTORY: " + WELL_KNOWN_DIRECTORY );
         SpringApplication.run( ElPuenteBoot.class, args );
     }
 }
