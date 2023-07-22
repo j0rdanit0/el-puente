d6a6d050c1a410e6a114bad454dfff8de9b21e43
JordanSimpson <jordan.simpson.dev@gmail.com>
2023-07-22 16:49:30 -0500

    Fix image issues

--- src/main/java/org/elpuentesearcy/controller/InvolvementController.java
+++ src/main/java/org/elpuentesearcy/controller/InvolvementController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.File;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 @Controller
@@ -29,12 +30,12 @@ public class InvolvementController
         String[] carouselImageNames = new File( ElPuenteBoot.IMAGE_DIRECTORY + carouselFolder ).list();
         if ( carouselImageNames != null && carouselImageNames.length > 0 )
         {
-            List<String> carouselImagePaths = Arrays
+            Set<String> carouselImagePaths = Arrays
               .stream( carouselImageNames )
               .map( name -> ElPuenteBoot.IMAGE_FOLDER + carouselFolder + name )
-              .collect( Collectors.toList() );
+              .collect( Collectors.toSet() );
 
-            model.addAttribute( "carouselImagePaths", carouselImagePaths );
+            model.addAttribute( "carouselImagePaths", new TreeSet<>( carouselImagePaths ) );
         }
 
         return "involvement";
