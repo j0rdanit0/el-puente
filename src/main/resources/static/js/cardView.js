4940a1f61cb5c6626b2ee287eef0a8d4fb105269
JordanSimpson <jordan.simpson.dev@gmail.com>
2023-09-30 14:52:37 -0500

    Added donation links to all the new Give Lively campaigns.

--- src/main/resources/static/js/cardView.js
+++ src/main/resources/static/js/cardView.js
@@ -1,20 +1,22 @@
 $(document).ready( function() {
    $('.displayInColumnsButton').click( function() {
-      $(this).addClass( 'text-primary' )
-         .closest('.cardViewContainer').each( function() {
+      $(this).addClass( 'text-primary' ).closest('.cardViewContainer').each( function() {
          $(this).find('.displayCards').addClass( 'card-columns' );
          $(this).find('.displayInRowsButton').removeClass( 'text-primary' );
-         $(this).find('.cardImageContainer').removeClass('col-md-4 col-lg-3').addClass('blendedCardImage');
+         $(this).find('.cardImageContainer').removeClass('col-md-4 col-lg-3').addClass('col-12 blendedCardImage');
+         $(this).find('.cardInfoContainer').addClass('col-12');
+         $(this).find('.cardGiveLivelyContainer').removeClass('col-xl-3 col-lg-4 col-12');
          $(this).find('.blendedCardContainer').addClass('blendedCard');
       });
    });
 
    $('.displayInRowsButton').click( function() {
-      $(this).addClass( 'text-primary' )
-         .closest('.cardViewContainer').each( function() {
+      $(this).addClass( 'text-primary' ).closest('.cardViewContainer').each( function() {
          $(this).find('.displayCards').removeClass( 'card-columns' );
          $(this).find('.displayInColumnsButton').removeClass( 'text-primary' );
-         $(this).find('.cardImageContainer').addClass('col-md-4 col-lg-3').removeClass('blendedCardImage');
+         $(this).find('.cardImageContainer').addClass('col-md-4 col-lg-3').removeClass('col-12 blendedCardImage');
+         $(this).find('.cardInfoContainer').removeClass('col-12');
+         $(this).find('.cardGiveLivelyContainer').addClass('col-xl-3 col-lg-4 col-12');
          $(this).find('.blendedCardContainer').removeClass('blendedCard');
       });
    });
